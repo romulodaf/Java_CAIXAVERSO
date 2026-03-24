a1cdcdf1118dc1931a8692276fd59bd5e12f9b57
Rômulo Daví <romulodaf@hotmail.com>
2026-03-23 21:25:09 -0300

    Iniciando ajustes na Aula 01.

---
+++ src/tecnicasDeProgramacao/aula01/LegacyCDB.java
@@ -0,0 +1,66 @@
+package tecnicasDeProgramacao.aula01;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class LegacyCDB {
+
+    private final String identificador;
+    private final Date dataDeCriacao;
+    private final BigDecimal valor;
+    private final int prazoEmDias;
+    private boolean estaAtivo;
+
+    public LegacyCDB(String identificador, Date dataDeCriacao, BigDecimal valor, int prazoEmDias) {
+        this.identificador = identificador;
+        this.dataDeCriacao = dataDeCriacao;
+        this.valor = valor;
+        this.prazoEmDias = prazoEmDias;
+        this.estaAtivo = true;
+    }
+
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public Date getDataDeCriacao() {
+        return dataDeCriacao;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public int getPrazoEmDias() {
+        return prazoEmDias;
+    }
+
+    public Date getDataVencimento() {
+        Date dataDeVencimento = dataDeCriacao;
+        long prazoEmHoras = prazoEmDias * 24L;
+        long prazoEmMinutos = prazoEmHoras * 60L;
+        long prazoEmSegundos = prazoEmMinutos * 60L;
+        long prazoEmMillisegundos = prazoEmSegundos * 1000L;
+        dataDeVencimento.setTime(dataDeCriacao.getTime() + prazoEmMillisegundos);
+        System.out.println("DataVencimento: " + dataDeVencimento);
+        return dataDeVencimento;
+    }
+
+    public boolean estaEncerrado(Date dataAtual){
+        return dataAtual.after(getDataVencimento());
+    }
+
+    public boolean fecharCdb(){
+        return estaAtivo = false;
+    }
+
+    public boolean estaAtivo() {
+        return estaAtivo;
+    }
+
+    @Override
+    public String toString() {
+        return "LegacyCDB{id: %s, dataCriacao: %s, valor: %s, prazoEmDias: %s, ativo: %s}"
+                .formatted(identificador, dataDeCriacao, valor, prazoEmDias, estaAtivo);
+    }
+}
---
+++ src/tecnicasDeProgramacao/aula01/TechKidsOpeningValidator.java
@@ -0,0 +1,32 @@
+package tecnicasDeProgramacao.aula01;
+
+import java.time.*;
+
+public class TechKidsOpeningValidator {
+
+    public static void main(String[] args) throws InterruptedException {
+        var validator = new TechKidsOpeningValidator();
+        var dataDeNascimento = LocalDate.of(2008, Month.DECEMBER, 15);
+        boolean podeCriarConta = validator.isEligibleForTechKids(dataDeNascimento, LocalDate.now());
+
+        var now =  LocalDateTime.now();
+        Thread.sleep(1000l);
+        var depois =  LocalDateTime.now();
+        Duration duration = Duration.between(now, depois);
+        System.out.println("Tempo de depois: " + duration);
+        System.out.println("Tempo de depois: " + duration.toSeconds());
+        if (podeCriarConta) {
+            System.out.println("Criando Conta");
+        } else {
+            System.out.println("Não pode criar a conta");
+        }
+    }
+
+    public boolean isEligibleForTechKids(LocalDate dataNascimento, LocalDate dataAtual) {
+        Period idadePeriod = Period.between(dataNascimento, dataAtual);
+        System.out.println("idadePeriod: " + idadePeriod);
+        int idade = idadePeriod.getYears();
+        System.out.println("Idade calculada: " + idade + " anos");
+        return idade < 18;
+    }
+}
